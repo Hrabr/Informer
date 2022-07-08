1f2d99be6f1d28fff9560e9b0d0bced6fb5f5cb6
Hrabr <emecandrei83@gmail.com>
2022-07-08 10:15:41 +0300

    add 29

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import api.controller.MyTimer;
 import api.controller.TelegramImplementations;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import java.io.IOException;
-
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         try {
@@ -19,5 +21,6 @@ public class Main {
         } catch (TelegramApiException e) {
             e.getStackTrace();
         }
-
-    }}
+        SpringApplication.run(Main.class,args);
+    }
+}
